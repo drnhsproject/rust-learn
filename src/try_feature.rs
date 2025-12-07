dfe707c1f48ab786b0f564458d18983e66dcf885
drnhsproject <dian.rahmat74@gmail.com>
2025-12-07 19:05:47 +0700

    feat: concurrent and pararel

--- src/try_feature.rs
+++ src/try_feature.rs
@@ -1,4 +1,5 @@
-use std::{thread, time::Duration};
+use std::{sync::Arc, thread, time::Duration};
+use tokio::runtime::Runtime;
 
 async fn get_async_data() -> String {
     thread::sleep(Duration::from_secs(2));
@@ -12,10 +13,25 @@ async fn get_database_data(wait: u64) -> String {
     return "Hello from database".to_string();
 }
 
+async fn run_concurrent(runtime: Arc<Runtime>) {
+    let mut handlers = vec![];
+
+    for i in 0..5 {
+        let handler = runtime.spawn(get_database_data(i));
+        handlers.push(handler);
+    }
+
+    for handler in handlers {
+        let result = handler.await.unwrap();
+        println!("Result: {}", result);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::time::Instant;
+    use tokio::runtime::{Builder, Runtime};
 
     #[tokio::test]
     async fn test_async() {
@@ -28,10 +44,11 @@ mod tests {
 
     #[tokio::test]
     async fn test_database() {
+        let runtime = Arc::new(Runtime::new().unwrap());
         let mut handlers = vec![];
 
         for i in 0..5 {
-            let handler = tokio::spawn(get_database_data(i));
+            let handler = runtime.spawn(get_database_data(i));
             handlers.push(handler);
         }
 
@@ -39,5 +56,18 @@ mod tests {
             let result = handler.await.unwrap();
             println!("Result: {}", result);
         }
+    }
+
+    #[test]
+    fn test_runtime() {
+        let runtime = Arc::new(
+            Builder::new_multi_thread()
+                .worker_threads(10)
+                .enable_time()
+                .build()
+                .unwrap(),
+        );
+
+        runtime.block_on(run_concurrent(Arc::clone(&runtime)));
     }
 }
