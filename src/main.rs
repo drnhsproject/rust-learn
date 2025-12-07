08993df4aab7579eb94dbd10007399c1bcdac0e8
drnhsproject <dian.rahmat74@gmail.com>
2025-12-07 15:15:32 +0700

    feat: mod tests in main

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,11 @@ fn main() {
     hello::say_hello("world");
     goodbye::say_goodbye();
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_say_hello() {
+        assert_eq!(hello::say_hello("world"), "Hello world!");
+    }
+}
