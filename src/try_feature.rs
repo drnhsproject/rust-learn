985f10cb86afda41057831245026a426015f168c
drnhsproject <dian.rahmat74@gmail.com>
2025-12-07 18:52:53 +0700

    feat: concurrent

--- src/try_feature.rs
+++ src/try_feature.rs
@@ -5,6 +5,13 @@ async fn get_async_data() -> String {
     return "Hello from async".to_string();
 }
 
+async fn get_database_data(wait: u64) -> String {
+    println!("{:?} : get database data", thread::current().id());
+    tokio::time::sleep(Duration::from_secs(wait)).await;
+    println!("{:?} : hello from database", thread::current().id());
+    return "Hello from database".to_string();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -17,5 +24,20 @@ mod tests {
         let duration = start.elapsed();
         println!("Result: {}", result);
         println!("Duration: {}", duration.as_secs());
+    }
+
+    #[tokio::test]
+    async fn test_database() {
+        let mut handlers = vec![];
+
+        for i in 0..5 {
+            let handler = tokio::spawn(get_database_data(i));
+            handlers.push(handler);
+        }
+
+        for handler in handlers {
+            let result = handler.await.unwrap();
+            println!("Result: {}", result);
+        }
     }
 }
